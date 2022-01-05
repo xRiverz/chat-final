efb5749c6df69d0bd7687a9fb5958b6fc5dc2b2e
xRiverz <anhargalam@gmail.com>
2022-01-05 16:03:05 +0000

    Initial Commit

---
+++ chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chat
+//
+//  Created by administrator on 05/01/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
