c286bc8693b27a6502a8d9c3ab862b7b45825723
xRiverz <anhargalam@gmail.com>
2022-01-06 13:24:48 +0000

    updated

---
+++ chat/Controllers/MessageViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MessageViewController.swift
+//  chat
+//
+//  Created by administrator on 06/01/2022.
+//
+
+import UIKit
+
+class MessageViewController : UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
